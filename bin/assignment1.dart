d1785a55938d1d4728421fab05941383ea8115e5
abdulkaderit <abdulkaderit95@gmail.com>
2025-09-15 19:45:29 +0600

    Book

--- bin/assignment1.dart
+++ bin/assignment1.dart
@@ -1,57 +1,32 @@
-import 'package:args/args.dart';
+class Book {
+  String title;
+  String author;
+  double price;
 
-const String version = '0.0.1';
+  // Constructor
+  Book(this.title, this.author, this.price);
 
-ArgParser buildParser() {
-  return ArgParser()
-    ..addFlag(
-      'help',
-      abbr: 'h',
-      negatable: false,
-      help: 'Print this usage information.',
-    )
-    ..addFlag(
-      'verbose',
-      abbr: 'v',
-      negatable: false,
-      help: 'Show additional command output.',
-    )
-    ..addFlag('version', negatable: false, help: 'Print the tool version.');
-}
-
-void printUsage(ArgParser argParser) {
-  print('Usage: dart assignment1.dart <flags> [arguments]');
-  print(argParser.usage);
-}
-
-void main(List<String> arguments) {
-  final ArgParser argParser = buildParser();
-  try {
-    final ArgResults results = argParser.parse(arguments);
-    bool verbose = false;
-
-    // Process the parsed arguments.
-    if (results.flag('help')) {
-      printUsage(argParser);
-      return;
-    }
-    if (results.flag('version')) {
-      print('assignment1 version: $version');
-      return;
-    }
-    if (results.flag('verbose')) {
-      verbose = true;
-    }
-
-    // Act on the arguments provided.
-    print('Positional arguments: ${results.rest}');
-    if (verbose) {
-      print('[VERBOSE] All arguments: ${results.arguments}');
-    }
-  } on FormatException catch (e) {
-    // Print usage information if an invalid argument was provided.
-    print(e.message);
-    print('');
-    printUsage(argParser);
+  // Method to calculate discounted price
+  double discountedPrice(double discountPercent) {
+    return price - (price * discountPercent / 100);
   }
+
+  // Method to display book details
+  void displayBookDetails(double discountPercent) {
+    print("Title: $title");
+    print("Author: $author");
+    print("Original Price: \$${price.toStringAsFixed(2)}");
+    print("Discounted Price ($discountPercent% off): \$${discountedPrice(discountPercent).toStringAsFixed(2)}");
+
+  }
+}
+
+void main() {
+  // Creating book objects
+  Book book1 = Book("Natun Chand", "Kazi Nazrul", 150.99);
+  Book book2 = Book("Nakshi Kanthar Math", "Jasimuddin", 250.50);
+
+  // Printing details with discount
+  book1.displayBookDetails(10); // 10% discount
+  book2.displayBookDetails(20); // 20% discount
 }
